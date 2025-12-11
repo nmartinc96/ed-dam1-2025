cc7d45078c99675aa6c10506b34a06009982a1e9
Natalia <natalia.marcos@jcyl.educa.es>
2025-12-11 19:27:30 +0100

    Task: generate a new class for entity facture. #closes4

---
+++ src/features/Factura.java
@@ -0,0 +1,51 @@
+package features;
+
+public class Factura {
+
+    int numeroFactura;
+    String concepto;
+    String precio;
+    String fecha;
+    String hora;
+
+    public int getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(int numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+}
